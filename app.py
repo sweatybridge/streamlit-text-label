3b67c55a73228b6717cfe7eac4af54925dd243e0
Han Qiao <qiao@basis-ai.com>
2021-05-03 21:30:33 +0800

    Add demo app

---
+++ app.py
@@ -0,0 +1,20 @@
+import lorem
+import streamlit as st
+
+from component import tooltip_select
+
+
+@st.cache
+def get_body():
+    return lorem.paragraph()
+
+
+def main():
+    st.title("Component Gallery")
+    st.header("Label selected text")
+    selected = tooltip_select(get_body(), ["Noun", "Verb"])
+    st.write(selected)
+
+
+if __name__ == "__main__":
+    main()
